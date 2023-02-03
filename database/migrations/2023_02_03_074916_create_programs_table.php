0dcb6aa4d37d36b29253db8099fb17516bc99538
Johan <jcgez777@gmail.com>
2023-02-03 15:11:39 -0500

    Primera-Version

---
+++ database/migrations/2023_02_03_074916_create_programs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->id('code');
+            $table->string('name');
+            $table->integer('version');
+            $table->string('estate');
+            $table->string('duration');
+            $table->string('competence');
+            $table->string('result');
+            $table->string('level_training');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('programs');
+    }
+};
